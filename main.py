5567dfc821c1b1afef5fac1e363250c8c9477fe7
Kat Morgan <kat@braincraft.io>
2023-08-15 17:16:15 -0700

    tossing an api server together

--- main.py
+++ main.py
@@ -2,13 +2,31 @@
 # Import necessary libraries
 import os
 from vllm import VLLMModel
+from vllm import VLLMServer
 
 # Get the base model directory from the environment variable
 MODEL_DIR = os.environ.get('MODEL_DIR', '/models')  # Default to '/models' if not set
 
 # Define the models to be served
 models = [
-    VLLMModel(name="LLaMA-2-7b-hf", path=os.path.join(MODEL_DIR, "path/to/model1")),
-    VLLMModel(name="GPT-2", path=os.path.join(MODEL_DIR, "path/to/model2")),
-    # Add more models as needed, using os.path.join(MODEL_DIR, "path/to/model")
+    VLLMModel(name="LLaMA-2-7b-hf", path=os.path.join(MODEL_DIR, "llama-2-7b-hf")),
+#   VLLMModel(name="LLaMA-2-13b-hf", path=os.path.join(MODEL_DIR, "llama-2-13b-hf")),
 ]
+
+# Configure the vLLM server
+server = VLLMServer(models=models, port=8000, workers=4)
+
+# Define API endpoints
+def inference(request):
+    model_name = request.get("model_name")
+    input_text = request.get("input_text")
+    model = server.get_model(model_name)
+    output = model.infer(input_text)
+    return {"output": output}
+
+def status(request):
+    return {"status": "OK"}
+
+# start the server
+if __name__ == "__main__":
+    server.start()
