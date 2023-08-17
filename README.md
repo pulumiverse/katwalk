395ec82ff50f2f03bcd8336983ecde1f5723046a
usrbinkat <usrbinkat@braincraft.io>
2023-08-17 04:58:32 +0000

    working api server

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # KatWalk
 A model deployment infrastructure repository
+
+```bash
+curl -X POST "http://localhost:8000/generate" -H "Content-Type: application/json" -d '{"prompt": "Once upon a time in a faraway land, ", "stream": false}'
+```
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.sampling_params import SamplingParams
 from vllm.utils import random_uuid
 
+MODEL_DIR = "/models"
 TIMEOUT_KEEP_ALIVE = 5  # seconds.
 TIMEOUT_TO_PREVENT_DEADLOCK = 1  # seconds.
 app = FastAPI()
