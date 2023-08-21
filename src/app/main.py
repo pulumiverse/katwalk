9b98f371604fa9a53a3fb2b5e5f9b26f90f3c2b1
usrbinkat <usrbinkat@braincraft.io>
2023-08-21 14:35:08 +0000

    tidy dockerfile and main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,20 +1,39 @@
-#!/usr/bin/env python3
-#!/usr/bin/env python3
 import argparse
 import json
+import re
 import torch
 from fastapi import BackgroundTasks, FastAPI, Request, Response
 from fastapi.responses import JSONResponse
 import uvicorn
 from vllm import LLM, SamplingParams
 
-MODEL_DIR = "/models"
+MODEL_DIR = "/model"
 app = FastAPI()
 
 # Create an instance of the LLM class using the model directory
 llm = LLM(MODEL_DIR)
 
-@app.post("/generate")
+# System instruction string
+SYS_INSTRUCTION = '''You are a helpful assistant.'''
+
+# Template
+TEMPLATE = '<s>[INST] <<SYS>> {{ system_prompt }} <</SYS>> {{ user_message }} [/INST]'
+
+def assemble_prompt(user_prompt):
+    # Strip unwanted characters
+    user_prompt = re.sub(r'[\n\"\']', '', user_prompt).strip()
+    
+    print("INFO: prompt_log | original_prompt >> " + user_prompt)
+
+    # Replace placeholders in the template with the SYS instruction and user prompt
+    final_prompt = TEMPLATE.replace('{{ system_prompt }}', SYS_INSTRUCTION)
+    final_prompt = final_prompt.replace('{{ user_message }}', json.dumps(user_prompt))
+    
+    # Log the assembled prompt
+    print("INFO: prompt_log | system_prompt " + final_prompt)
+    return final_prompt
+
+@app.post("/v1/chat")
 async def generate(request: Request) -> Response:
     body = await request.body()
     print("Request Body:", body.decode())  # Print the request body
@@ -24,11 +43,12 @@ async def generate(request: Request) -> Response:
     except json.JSONDecodeError:
         return JSONResponse({"error": "Malformed JSON"}, status_code=400)
 
-    prompt = request_dict.pop("prompt")
+    user_prompt = request_dict.pop("prompt")
+    final_prompt = assemble_prompt(user_prompt)
     sampling_params = SamplingParams(**request_dict)
 
     # Generate the results using the LLM instance
-    result = llm.generate([prompt], sampling_params)  # Pass prompt as a list
+    result = llm.generate([final_prompt], sampling_params)  # Pass prompt as a list
 
     # Extract the text from the result
     text_outputs = [output.text for output in result[0].outputs]
