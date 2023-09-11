7c9d633ec63a849d0c2b9c0c35bb4d4fded0a9b3
Adora Nwodo <adoranwodo@Adoras-MacBook-Pro.local>
2023-09-11 21:10:53 +0100

    Add root directory

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -249,6 +249,7 @@ project = vercel.Project("vercel-project",
         repo = repoName,
         type = repoType
     ),
+    root_directory = "./src/app/katwalk-frontend",
     opts = pulumi.ResourceOptions(
         provider = provider
     )
